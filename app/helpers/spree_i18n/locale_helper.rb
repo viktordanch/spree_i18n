757cde03c895ddacd8fd6c759ca6e2e339b258a3
viktordanch <viktor.danch@gmail.com>
2017-04-29 10:28:59 +0300

    fix locales

--- app/helpers/spree_i18n/locale_helper.rb
+++ app/helpers/spree_i18n/locale_helper.rb
@@ -15,7 +15,10 @@ module SpreeI18n
     # Need to manually add en to the array because the en.yml was moved from
     # this project. spree/spree now has those keys.
     def all_locales_options
-      SpreeI18n::Locale.all.map { |locale| locale_presentation(locale) }.push(['English (EN)', :en])
+      # SpreeI18n::Locale.all.map { |locale| locale_presentation(locale) }.push(['English (EN)', :en])
+      dir = File.join(File.dirname(__FILE__), '/../../../config/locales')
+      all = Dir["#{dir}/*.yml"].map { |f| File.basename(f, '.yml').to_sym }
+      all.map { |locale| locale_presentation(locale) }.push(['English (EN)', :en])
     end
 
     private
